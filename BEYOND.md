3218b356e626a7cc5e4cd78e9d1e8a054a6bc7c8
Juliette404-error <yoonj6914@my.sduhsd.net>
2026-04-10 07:15:50 +0000

    designed a lot! (included memes)

---
+++ BEYOND.md
@@ -0,0 +1,6 @@
+# Above and Beyond Features
+
+## CSS Styling
+- **What:** added background color, styled buttons (including making the text match the page's font family, not browser default), 
+- **Where:** Used "style.css" as my file
+- **Why:** Makes the game look polished and professional, especially user-friendly
--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// add javascript here
+// basic set-up variables
 let guess = 0;
 let answer = 0;
 let guessCount = 0;
 let totalWins = 0;
 let range = 0;
+console.log(guess, answer, guessCount, totalWins, range)
 
+const scores = [];
+console.log(scores)
+
+// name
 let yourName = prompt("Enter your name ");
 let formattedName = yourName.charAt(0).toUpperCase() + yourName.slice(1).toLowerCase(); // changes first letter to uppercase; changes rest into lowercase
+console.log(yourName, formattedName)
 
 
+// date and clock
 function time() {
     let now = new Date();
     let day = now.getDate();
@@ -21,19 +28,28 @@ function time() {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     let suffix;
-    if ([11,12,13].includes(day)) suffix = "th";
-    else if (day % 10 === 1) suffix = "st";
-    else if (day % 10 === 2) suffix = "nd";
-    else if (day % 10 === 3) suffix = "rd";
-    else suffix = "th";
+    if ([11,12,13].includes(day)){
+        suffix = "th";
+    }
+    else if (day % 10 === 1){
+        suffix = "st";
+    }
+    else if (day % 10 === 2){
+        suffix = "nd";
+    }
+    else if (day % 10 === 3){
+        suffix = "rd";
+    }
+    else{
+        suffix = "th";
+    }
 
     document.getElementById("date").textContent = months[month] + " " + day + suffix + ", " + year + " " + hours + ":" + String(minutes).padStart(2,'0') + ":" + String(seconds).padStart(2,'0');
 }
 time();
 setInterval(time, 1000);
 
-const scores = [];
-
+// basic set-up for HTML linkages
 const msg = document.getElementById("msg");
 const wins = document.getElementById("wins");
 const avgScore = document.getElementById("avgScore");
@@ -46,10 +62,20 @@ const e = document.getElementById("e");
 const m = document.getElementById("m");
 const h = document.getElementById("h");
 
+// images
+const reveal = document.getElementById("memeImg"); // refers to selecting image in html
+const memes = [".images/21.jpg", ".images/zokunmoz8mi81.jpg", ".images/zewcs60sv6m41.jpg", ".images/ig5u8ke5qo421.png", ".images/ug9hs18m1d831.png", ".images/1643287537640.jpg", ".images/doge_coding_meme.png", ".images/0_kjcbVX8Y9TFoHGDO.png", ".images/programmerhumor-io-programming-memes-frontend-memes-fbba4c4b7ad4701.png", ".images/EXfR6mLXQAID82r.jpg", ".images/programmerhumor-io-programming-memes-6aa5e5e99ca23c3.jpg", ".images/495294327_1947166475817605_783074843163109608_n.jpg", ".images/1_BMdNGcek6kqR5J5pwjQwAw.png", ".images/0_gUmnBbPXyTNTL8hI.jpg", ".images/erroronline42-v0-sk3ae6an0hgd1.webp", ".images/8cdd0ucxrra71.jpg", ".images/r_4394683_eSULD.jpg"]
+console.log(memes)
+
+
+// timer
 const times = [];
+console.log(times)
+
 let timerId = null;
 let fastestTime = Infinity; // first win = always "fastest"
 let timeSum = 0;
+console.log(timerId, fastestTime, timeSum)
 
 function runTimer() {
     let startTime = new Date().getTime();
@@ -64,18 +90,23 @@ function runTimer() {
 function stopTimer() {
     clearInterval(timerId);
     let finalTime = parseFloat(myTimer.textContent);
+    console.log("Final time:", finalTime);
     return finalTime;
 }
 
+
+// additional functions
 function updateScore(score){
     scores.push(score);
     wins.textContent = "Total wins: " + scores.length;
+    console.log("Scores array:", scores, "Total wins:", scores.length);
 
     let sum = 0;
     for(let i = 0; i < scores.length; i++){ // remember, NOT i <= scores.length because scores.length would have empty slot since i starts at 0
         sum += scores[i];
     }
     avgScore.textContent = "Average score: " + (sum/scores.length).toFixed(1) // shows scores with one decimal place
+    console.log("Average score:", (sum/scores.length).toFixed(1));
 
     scores.sort(function(a,b){return a-b;}); // to sort scores by ASCII character
 
@@ -104,6 +135,8 @@ function resetGame(){
 
 guessInput.disabled = true;
 
+
+// play button function
 playBtn.addEventListener("click", play);
 function play(){
     let levels = document.getElementsByName("level") // .getElementsByName = gets all elements from HTML with "[name]"
@@ -116,6 +149,15 @@ function play(){
     msg.textContent = "Hello, " + formattedName + "! Guess a number 1-" + range + ".";
     answer = Math.floor(Math.random()*range) + 1;
     guessCount = 0;
+    console.log(answer)
+    console.log(guessCount)
+
+    let randomIndex = Math.floor(Math.random() * memes.length); // randomizes mystery image whenever new game starts (based on how many images are there in array) --> similar to how mystery number is randomized (see above)
+    document.getElementById("memeImg").src = memes[randomIndex];
+    console.log("Random image index:", randomIndex, "Image:", memes[randomIndex]);
+
+    reveal.className = "";  // clear previous blur level of image
+    reveal.className = "blur20";  // sets default blur
 
     guessInput.disabled = false;
     guessBtn.disabled = false;
@@ -125,6 +167,7 @@ function play(){
     runTimer();
     }
 
+// guess button function --> MAIN PART OF GAME
 guessBtn.addEventListener("click", makeGuess);
 function makeGuess(){
     let guess = parseInt(guessInput.value);
@@ -134,50 +177,77 @@ function makeGuess(){
     }
 
     guessCount++;
+    console.log("Guess:", guess, "Answer:", answer, "Difference:", Math.abs(guess - answer), "Guess count:", guessCount);
+
     if(guess == answer){
         msg.textContent = "Correct! You guessed it! It took " + guessCount + " tries, " + formattedName + "!";
         if(guessCount == 1){
             msg.textContent = "Correct! You guessed it! It took 1 try (you're a good guesser, " + formattedName + ").";
-    }
+    
+        }
 
-    let finalTime = stopTimer();
-    times.push(finalTime);
-    if (finalTime < fastestTime) {
-        fastestTime = finalTime;
-        document.getElementById("fastest").textContent = "Fastest Game: " + fastestTime.toFixed(3) + "s";
-    }
+        let finalTime = stopTimer();
+        times.push(finalTime);
+        if (finalTime < fastestTime){
+            fastestTime = finalTime;
+            document.getElementById("fastest").textContent = "Fastest Game: " + fastestTime.toFixed(3) + "s";
+        }
 
-    timeSum = 0;
-    for(let i = 0; i < times.length; i++) {
-        timeSum += times[i];
-    }
-    document.getElementById("avgTime").textContent = "Average Time: " + (timeSum / times.length).toFixed(3) + "s";
+        timeSum = 0;
+        for(let i = 0; i < times.length; i++){
+            timeSum += times[i];
+        }
+        document.getElementById("avgTime").textContent = "Average Time: " + (timeSum / times.length).toFixed(3) + "s";
+        console.log("Times array:", times, "Fastest:", fastestTime, "Avg time:", (timeSum / times.length).toFixed(3));
 
-    updateScore(guessCount);
-    resetGame();
+        reveal.className = "";
+        reveal.className = "blur0";
+        
+        updateScore(guessCount);
+        resetGame();
     }
     
     else if(Math.abs(guess - answer) <= 2 && guess > answer){ // if guess is 2 more/less than answer & guess > answer
         msg.textContent = "HOT! IT'S HOT! You're guess is really close! Just a little too HIGH.";
+        
+        reveal.className = "";
+        reveal.className = "blur5";
     }
     else if(Math.abs(guess - answer) <= 2 && guess < answer){ // if guess is 2 more/less than answer & guess < answer
         msg.textContent = "HOT! IT'S HOT! You're guess is really close! Just a little too LOW.";
+        
+        reveal.className = "";
+        reveal.className = "blur5";
     }
     else if(Math.abs(guess - answer) <= 5 && guess > answer){ // if guess is 10 more/less than answer & guess > answer
         msg.textContent = "Getting warmer ... You're still a little too HIGH.";
+
+        reveal.className = "";
+        reveal.className = "blur10";
     }
     else if(Math.abs(guess - answer) <= 5 && guess < answer){ // if guess is 10 more/less than answer & guess < answer
         msg.textContent = "Getting warmer ... You're still a little too LOW.";
+    
+        reveal.className = "";
+        reveal.className = "blur10";
     }
     else if(guess > answer){
         msg.textContent = "Brr! You're cold! Your guess is too HIGH. Try again!";
+    
+        reveal.className = "";
+        reveal.className = "blur20";
     }
     else{
         msg.textContent = "Brr! You're cold! Your guess is too LOW. Try again!";
+    
+        reveal.className = "";
+        reveal.className = "blur20";
     }
     
 }
 
+
+// give up button function
 giveUpBtn.addEventListener("click", giveUp);
 function giveUp(){
     msg.textContent = "The answer is: " + answer + ". It took " + guessCount + " tries before you gave up.";
@@ -185,3 +255,10 @@ function giveUp(){
     updateScore(range);
     resetGame();
 }
+
+
+// dark mode toggle function
+document.getElementById("darkModeBtn").addEventListener("click", function() {
+    document.body.classList.toggle("dark"); // goes to CSS and activates functions w/ .dark
+    this.textContent = document.body.classList.contains("dark") ? "Light Mode" : "Dark Mode";
+});
